fed0f96d6842e6dcc15d60ec728fc5170ec976c2
sonu267 <65347096+sonu267@users.noreply.github.com>
2024-02-18 16:05:18 +0530

    Add files via upload

---
+++ table multiplayer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("enter you number you want multiplacation of table");
+    scanf("%d", &num);
+    printf("%d x 1 = %d \n ",num, num*1 );
+    printf("%d x 2 = %d \n ", num,num*2 );
+    printf("%d x 3 = %d \n ", num,num*3 );
+    printf("%d x 4 = %d \n ", num,num*4 );
+    printf("%d x 5 = %d \n ", num,num*5 );
+    printf("%d x 6 = %d \n ", num,num*6 );
+    printf("%d x 7 = %d \n ", num,num*7 );
+    printf("%d x 8 = %d \n ", num,num*8 );
+    printf("%d x 9 = %d \n ", num,num*9 );
+    printf("%d x 10 = %d", num,num*10 );
+    return 0;
+
+}
